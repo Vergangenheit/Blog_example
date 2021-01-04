fa6ac22a2c1995e63e733d3700210b40222f4517
Vergangenheit <lorenzo.ostano@gmail.com>
2021-01-04 09:54:08 +0100

    first commit

---
+++ flask_blog/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    MAIL_SERVER = 'smtp.mail.yahoo.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'lorenzo_ostano@yahoo.com'
+    MAIL_PASSWORD = 'Lo752672$'
